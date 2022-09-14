b581b9ffb090e35ff28a25534855db86ff39d2d3
mikedelcastillo <johnmichaeldc@gmail.com>
2022-09-15 00:13:22 +0800

    Added routes to express

---
+++ packages/core/src/lib/express.ts
@@ -0,0 +1,33 @@
+import { ErrorRequestHandler, NextFunction, Request, Response, RequestHandler } from "express"
+import createError from "http-errors"
+
+export const handle404Error: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
+    next(createError(404))
+}
+
+export const handleError: ErrorRequestHandler = (err, req: Request, res: Response, next: NextFunction) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get("env") === "development" ? err : {}
+
+    // render the error page
+    const status = err.status || 500
+    let message = err.message
+    if(process.env.NODE_ENV !== "production" && status >= 500){
+        message = "Something went wrong"
+    }
+
+    res.status(status)
+    res.json({
+        ok: false,
+        message,
+    })
+
+    console.warn(err)
+}
+
+export type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<void>
+
+export const asyncHandler = (handler: AsyncHandler) => 
+    (req: Request, res: Response, next: NextFunction) => 
+        handler(req, res, next).catch(next)
--- packages/core/src/networking/GameServer.ts
+++ packages/core/src/networking/GameServer.ts
@@ -1,16 +1,28 @@
 import http from "http"
-import express, { Express } from "express"
+import express, { Express, Request, Response, NextFunction } from "express"
+import { asyncHandler, handle404Error, handleError } from "../lib/express"
 
 export class GameServer{
-    private app: Express
-    private server: http.Server
+    app: Express
+    server: http.Server
 
     constructor(){
         this.app = express()
         this.server = http.createServer(this.app)
+
+        this.app.get("/", asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+            res.json({
+                ok: true
+            })
+        }))
     }
 
     async start(){
+        // Add error handler before starting server
+        this.app.use(handle404Error)
+        this.app.use(handleError)
+
+        // Start the server
         await (new Promise(resolve => {
             this.server.listen(3000, () => resolve(null))
         }))
