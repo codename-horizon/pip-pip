a572de5479220b0669d99a401396d1dc9346a44b
mikedelcastillo <johnmichaeldc@gmail.com>
2022-09-15 00:22:19 +0800

    Testing express options

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,6 +2,10 @@ import { GameServer } from "@pip-pip/core";
 
 const gameServer = new GameServer()
 
+gameServer.app.get("/test", (req, res) => {
+    res.json({ok: true})
+})
+
 gameServer.start().then(() => {
     console.log("Server started")
 })
