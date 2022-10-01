a4bda5e2e9df66aa1ad05277b298bcbf7dbe5c61
Mike del Castillo <mikedelcastillo@MegMike-Wendy.local>
2022-10-02 00:06:15 +0800

    Successful test of number encoding

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,5 +1,5 @@
 type PacketSerializer<T = any> = {
-    length: number,
+    readonly length: number,
     encode: (value: T) => Uint8Array,
     decode: (value: Uint8Array) => T,
 }
@@ -40,37 +40,128 @@ const float64 = numPacket("float64")
 
 type PacketInputMap = Record<string, unknown>
 type PacketSerializerMap = Record<string, PacketSerializer>
-class Pac<T extends PacketSerializerMap>{
+
+type GetPacketInput<T extends PacketSerializerMap> = {
+    [K in keyof T]: T[K] extends PacketSerializer<infer R> ? R : never
+}
+class Packet<T extends PacketSerializerMap>{
     id: number
-    dataTypes: T
-    constructor(id: number, dataTypes: T){
+    serializers: T
+    keyOrder: string[] = []// Bugs out if Array<keyof T>
+    messageLength = 0
+
+    constructor(id: number, serializers: T){
         if(id < 0 || id > 255) throw new Error("ID must be an unsigned int8.")
         this.id = id
-        this.dataTypes = dataTypes
+        this.serializers = serializers
+        
+        for(const key in serializers){
+            const serializer = serializers[key]
+            this.keyOrder.push(key)
+            this.messageLength += serializer.length
+        }
+
+        this.keyOrder = this.keyOrder.sort()
+    }
+
+    encode<I extends GetPacketInput<T>>(inputs: I | I[]){
+        const output = [this.id]
+
+        if(!Array.isArray(inputs)) inputs = [inputs]
+
+        for(const inp of inputs){
+            for(const key of this.keyOrder){
+                const value = inp[key]
+                const arr = this.serializers[key].encode(value)
+                output.push(...arr)
+            }
+        }
+
+        return output.map(c => String.fromCharCode(c)).join("")
+    }
+
+    decode(value: string){
+        const input = value.split("").map(s => s.charCodeAt(0))
+        
+        if(input[0] !== this.id) throw new Error("Cannot decode this message. Wrong ID.")
+
+        input.shift()
+
+        if(input.length % this.messageLength !== 0) throw new Error("Message missing data.")
+
+        const messageCount = input.length / this.messageLength
+        const output = []
+
+        for(let i = 0; i < messageCount; i++){
+            let indexOffset = i * this.messageLength
+            const obj: Record<string, any> = {}
+            for(const key of this.keyOrder){
+                const serializer = this.serializers[key]
+                obj[key] = serializer.decode(new Uint8Array(input.slice(indexOffset, indexOffset + serializer.length)))
+                indexOffset += serializer.length
+            }
+            output.push(obj)
+        }
+
+        return output
     }
 }
 
 type PacketManagerInputMap = Record<string, PacketInputMap>
 
-type GamePacketInputMap = {
-    "playerPositions": {
-        x: number,
-        y: number,
-    }
-}
-
 type PacketManagerInputSerializerMap<T extends PacketManagerInputMap> = {
-    [K in keyof T]: Pac<{
+    [K in keyof T]: Packet<{
         [P in keyof T[K]]: PacketSerializer<T[K][P]>
     }>
 }
+type PacketManagerSerialierMap = Record<string, Packet<PacketSerializerMap>>
 
-type GamePacketInputSerializerMap = PacketManagerInputSerializerMap<GamePacketInputMap>
-
-const packets: GamePacketInputSerializerMap = {
-    "playerPositions": new Pac(0, {
-        x: uint16,
-        y: uint16,
+const packets = {
+    "playerPositions": new Packet(0, {
+        x: float32,
+        y: float32,
+        a: float32,
+        b: float32,
+        s: uint16,
+        r: uint8,
     }),
 }
 
+type GetPacketInputs<T extends PacketManagerSerialierMap> = {
+    [K in keyof T]: T[K] extends Packet<infer R> ? {
+        [I in keyof R]: R[I] extends PacketSerializer<infer J> ? J : never
+    } : never
+}
+
+type T = GetPacketInputs<typeof packets>
+
+class PacketManager<T extends PacketManagerSerialierMap>{
+    packets: T
+    constructor(packets: T){
+        this.packets = packets
+    }
+
+    encode<
+        K extends keyof T,
+        O extends GetPacketInputs<T>,
+        P extends O[K]
+    >(id: K, inputs: P | P[]){
+        return this.packets[id].encode(inputs)
+    }
+}
+
+const pm = new PacketManager(packets)
+
+const example = Array(2).fill({
+    x: Math.PI,
+    y: Math.PI / 2,
+    a: Math.PI * 2,
+    b: 69,
+    s: 696,
+    r: 6,
+})
+
+const a = pm.encode("playerPositions", example)
+const code = packets.playerPositions.encode(example)
+const decode = packets.playerPositions.decode(code)
+console.log(packets.playerPositions.messageLength, a, code, decode)
